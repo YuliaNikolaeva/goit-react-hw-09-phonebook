d98ec4c6618252c291e2fada9ff5ecd3c4d0a488
Yulia Nikolaeva <y.nikolaeva@rndev.org>
2020-08-30 08:26:15 +0300

    HW 09, part 2, fix-1

--- src/redux/contacts/contacts-operations.js
+++ src/redux/contacts/contacts-operations.js
@@ -16,7 +16,7 @@ const {
 const fetchContacts = () => dispatch => {
     dispatch(fetchContactsRequest());
 
-    axios.get('/contacts11')
+    axios.get('/contacts22222')
     .then(response => dispatch(fetchContactsSuccess(response.data)))
     .catch(err => dispatch(fetchContactsError(err.message)))
 };
